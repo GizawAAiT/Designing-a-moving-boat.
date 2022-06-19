a85c31bdefe28d800e17136ccdc28acad4ad7250
kalish93 <91588962+kalish93@users.noreply.github.com>
2022-06-19 10:57:50 +0300

    Update boat.py

--- boat.py
+++ boat.py
@@ -2,120 +2,87 @@ import glfw
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import pyrr
-from TextureLoader import load_texture
+from TextureLoader import texture_loader
 from OBJLoader import ObjFileLoader
+import os
 
-
-vertex_src = """
-# version 330
-
-layout(location = 0) in vec3 a_position;
-layout(location = 1) in vec2 a_texture;
-layout(location = 2) in vec3 a_normal;
-
-uniform mat4 model;
-uniform mat4 projection;
-uniform mat4 view;
-
-out vec2 v_texture;
-
-void main()
-{
-    gl_Position = projection * view * model * vec4(a_position, 1.0);
-    v_texture = a_texture;
-}
-"""
-
-fragment_src = """
-# version 330
-
-in vec2 v_texture;
-
-out vec4 out_color;
-
-uniform sampler2D s_texture;
-
-void main()
-{
-    out_color = texture(s_texture, v_texture);
-}
-"""
-
-
-# glfw callback functions
+def getFileContents(filename):
+    p = os.path.join(os.getcwd(), "shader", filename)
+    return open(p, 'r').read()
+VertexShaderContent = getFileContents("vertex.shader")
+FragmentShaderContent = getFileContents("fragment.shader")
+    
 def window_resize(window, width, height):
     glViewport(0, 0, width, height)
     projection = pyrr.matrix44.create_perspective_projection_matrix(45, width / height, 0.1, 100)
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
 
-
-# initializing glfw library
 if not glfw.init():
     raise Exception("glfw can not be initialized!")
 
-# creating the window
-window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
+window = glfw.create_window(800, 600, "My OpenGL window", None, None)
 
-# check if window was created
 if not window:
     glfw.terminate()
     raise Exception("glfw window can not be created!")
 
-# set window's position
-glfw.set_window_pos(window, 0, 0)
 
+glfw.set_window_pos(window, 2, 2)
 
-# set the callback function for window resize
 glfw.set_window_size_callback(window, window_resize)
 
-# make the context current
 glfw.make_context_current(window)
 
-# load here the 3d meshes
-boat_indices, boat_buffer = ObjFileLoader.load_model("boat.obj")
-# monkey_indices, monkey_buffer = ObjLoader.load_model("meshes/monkey.obj")
 
-shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
+boat_indices, boat_buffer = ObjFileLoader.model_loader("last1.obj")
+water_indices, water_buffer = ObjFileLoader.model_loader("water1.obj")
 
-# VAO and VBO
+shader = compileProgram(compileShader(VertexShaderContent, GL_VERTEX_SHADER), compileShader(FragmentShaderContent, GL_FRAGMENT_SHADER))
+
+
 VAO = glGenVertexArrays(2)
 VBO = glGenBuffers(2)
-EBO = glGenBuffers(1)
-print(len(boat_indices)*3*4,boat_indices,boat_buffer)
-# Chibi VAO
 glBindVertexArray(VAO[0])
-# Chibi Vertex Buffer Object
+
 glBindBuffer(GL_ARRAY_BUFFER, VBO[0])
 glBufferData(GL_ARRAY_BUFFER, boat_buffer.nbytes, boat_buffer, GL_STATIC_DRAW)
 
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
-glBufferData(GL_ELEMENT_ARRAY_BUFFER, boat_indices.nbytes, boat_indices, GL_STATIC_DRAW)
-
-# chibi vertices
 glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, boat_buffer.itemsize * 8, ctypes.c_void_p(0))
-# chibi textures
+
 glEnableVertexAttribArray(1)
 glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, boat_buffer.itemsize * 8, ctypes.c_void_p(127152))
-# chibi normals
+
 glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, boat_buffer.itemsize * 8, ctypes.c_void_p(127152))
 glEnableVertexAttribArray(2)
 
+glBindVertexArray(VAO[1])
 
-# textures = glGenTextures(2)
-# load_texture("metal.jpg", textures[0])
+glBindBuffer(GL_ARRAY_BUFFER, VBO[1])
+glBufferData(GL_ARRAY_BUFFER, water_buffer.nbytes, water_buffer, GL_STATIC_DRAW)
+
+
+glEnableVertexAttribArray(0)
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, water_buffer.itemsize * 8, ctypes.c_void_p(0))
+glEnableVertexAttribArray(1)
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, water_buffer.itemsize * 8, ctypes.c_void_p(127152))
+
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, water_buffer.itemsize * 8, ctypes.c_void_p(127152))
+glEnableVertexAttribArray(2)
+textures = glGenTextures(2)
+texture_loader("wood.jpg", textures[0])
+texture_loader("ocean.jpg", textures[1])
 
 glUseProgram(shader)
-glClearColor(0, 0.1, 0.1, 1)
+glClearColor(0, 0, 0, 1)
 glEnable(GL_DEPTH_TEST)
 glEnable(GL_BLEND)
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
 projection = pyrr.matrix44.create_perspective_projection_matrix(45, 1280 / 720, 0.1, 100)
-chibi_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, -5, -10]))
-monkey_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([-4, 0, 0]))
+water_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, -5, -10]))
+boat_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([-4, 0, 0]))
 
-# eye, target, up
 view = pyrr.matrix44.create_look_at(pyrr.Vector3([0, 0, 8]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 1, 0]))
 
 model_loc = glGetUniformLocation(shader, "model")
@@ -125,24 +92,49 @@ view_loc = glGetUniformLocation(shader, "view")
 glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
 glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
 
+
+
+def move_x(obj,speed):
+    obj = pyrr.matrix44.create_from_translation(pyrr.Vector3([speed * glfw.get_time(),0, 0]))
+    return obj
+
+def move_z(obj,speed):
+    obj = pyrr.matrix44.create_from_translation(pyrr.Vector3([0,speed * glfw.get_time(),0]))
+    return obj
+
+def move_y(obj,speed):
+    obj = pyrr.matrix44.create_from_translation(pyrr.Vector3([0,0,speed * glfw.get_time()]))
+    return obj
+    
+
 while not glfw.window_should_close(window):
     glfw.poll_events()
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    rot_y = pyrr.Matrix44.from_y_rotation(0.8 * glfw.get_time())
-    model = pyrr.matrix44.multiply(rot_y, chibi_pos)
+    rot_y = pyrr.Matrix44.from_y_rotation(20)
+    model = pyrr.matrix44.multiply(rot_y, water_pos)
+
+    
+    boat_pos = move_x(boat_pos,1)
+    print(boat_pos)
 
     glBindVertexArray(VAO[0])
+    glBindTexture(GL_TEXTURE_2D, textures[0])
     glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
     glDrawArrays(GL_TRIANGLES, 0, len(boat_indices))
-    glDrawElements(GL_TRIANGLES, len(boat_indices), GL_UNSIGNED_INT, None)
 
-    rot_y = pyrr.Matrix44.from_y_rotation(-0.8 * glfw.get_time())
-    model = pyrr.matrix44.multiply(rot_y, monkey_pos)
-
+    rot_y = pyrr.Matrix44.from_y_rotation(90)
+    model = pyrr.matrix44.multiply(rot_y, boat_pos)
+    
+    glBindVertexArray(VAO[1])
+    glBindTexture(GL_TEXTURE_2D, textures[1])
+    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
+    glDrawArrays(GL_TRIANGLES, 0, len(water_indices))
+ 
   
 
     glfw.swap_buffers(window)
 
 glfw.terminate()
+
